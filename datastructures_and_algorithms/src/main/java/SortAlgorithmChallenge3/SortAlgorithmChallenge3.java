9fc8b098b9f7b9721abafa3eda8acd60af8b4132
Daniel <ddiegow@uoc.edu>
2023-02-27 15:07:48 +0900

    Finish search algorithm challenge 1, create skeletons for challenges 2 and 3

---
+++ datastructures_and_algorithms/src/main/java/SortAlgorithmChallenge3/SortAlgorithmChallenge3.java
@@ -0,0 +1,16 @@
+package SortAlgorithmChallenge3;
+
+/**
+ * Radix sort the strings
+ */
+public class SortAlgorithmChallenge3 {
+    public static void main(String[] args) {
+        String[] stringsArray = { "bcdef", "dbaqc", "abcde", "omadd", "bbbbb"};
+
+        // do radix sort
+
+        for (int i = 0; i < stringsArray.length; i++) {
+            System.out.println(stringsArray[i]);
+        }
+    }
+}
